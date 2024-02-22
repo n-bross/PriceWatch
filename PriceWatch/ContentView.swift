882796814e411b10a623523705649bebe550c53b
n-bross <miry_cookie0e@icloud.com>
2024-02-22 14:26:49 +0100

    Initilized Project

---
+++ PriceWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PriceWatch
+//
+//  Created by Nico Bro on 22.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PriceWatch/PriceWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceWatchApp.swift
+//  PriceWatch
+//
+//  Created by Nico Bro on 22.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PriceWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
